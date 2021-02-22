d65623d747d0b7e0e5eaa5cacad22c833c4966a6
456744 <ben@kinnaird.org.uk>
2021-02-22 12:45:37 +0000

    moved button sprites to separate file and added test scripts

---
+++ basil and poppy's flower garden project/Assets/scripts/testScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testScript : MonoBehaviour
+{
+    //string variable to use for other stuff in unity
+    public string startText;
+    public string updateText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.LogWarning(startText);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.LogError(updateText);
+    }
+}
