fa74a8f5e178432d6a48911d20a9cc4cc4b30d6d
456744 <ben@kinnaird.org.uk>
2021-03-01 12:32:30 +0000

    added buttons to UI input player sprite and created script for movement

---
+++ basil and poppy's flower garden project/Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //public variable, visable in unity editor
+    public float forceStrength;
+
+    //these will be called by each button for movement
+    public void MoveUp()
+    {
+        //get the rigidbody for movement
+        Rigidbody2D ourRigidbody = GetComponent<Rigidbody2D>();
+
+        //Move in the correct direction with the set force strength
+        ourRigidbody.AddForce(Vector2.up * forceStrength);
+    }
+    public void MoveDown()
+    {
+        //get the rigidbody for movement
+        Rigidbody2D ourRigidbody = GetComponent<Rigidbody2D>();
+
+        //Move in the correct direction with the set force strength
+        ourRigidbody.AddForce(Vector2.down * forceStrength);
+    }
+    public void MoveLeft()
+    {
+        //get the rigidbody for movement
+        Rigidbody2D ourRigidbody = GetComponent<Rigidbody2D>();
+
+        //Move in the correct direction with the set force strength
+        ourRigidbody.AddForce(Vector2.left * forceStrength);
+    }
+    public void MoveRight()
+    {
+        //get the rigidbody for movement
+        Rigidbody2D ourRigidbody = GetComponent<Rigidbody2D>();
+
+        //Move in the correct direction with the set force strength
+        ourRigidbody.AddForce(Vector2.right * forceStrength);
+    }
+}
