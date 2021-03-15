3bc7381bd80131d42fd11bf340f0fea45ee55c07
456744 <ben@kinnaird.org.uk>
2021-03-15 12:10:01 +0000

    added background music and directional movement animations

--- basil and poppy's flower garden project/Assets/scripts/PlayerMovement.cs
+++ basil and poppy's flower garden project/Assets/scripts/PlayerMovement.cs
@@ -40,4 +40,22 @@ public class PlayerMovement : MonoBehaviour
         //Move in the correct direction with the set force strength
         ourRigidbody.AddForce(Vector2.right * forceStrength);
     }
+
+    public void Update()
+    {
+        // Get the rigidbody from our player so we can check its speed
+        Rigidbody2D ourRigidbody = GetComponent<Rigidbody2D>();
+
+        // Find out from the rigidbody what our current horizontal and vertical speeds are
+        float currentSpeedH = ourRigidbody.velocity.x;
+        float currentSpeedV = ourRigidbody.velocity.y;
+
+        // Get the animator that we'll be using for movement
+        Animator ourAnimator = GetComponent<Animator>();
+
+        // Tell our animator what the speeds are
+        ourAnimator.SetFloat("speedH", currentSpeedH);
+        ourAnimator.SetFloat("speedV", currentSpeedV);
+
+    }
 }
