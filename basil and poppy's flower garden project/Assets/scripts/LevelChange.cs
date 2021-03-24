e0d921276cf5c125258e419bb55112f24bd13bb6
456744 <ben@kinnaird.org.uk>
2021-03-24 01:08:53 +0000

    added level change scripts and colliders to activate script

---
+++ basil and poppy's flower garden project/Assets/scripts/LevelChange.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChange : MonoBehaviour
+{
+
+    public string levelToLoad;
+
+    private void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+
+        if (otherCollider.tag == "Player")
+        {
+            SceneManager.LoadScene(levelToLoad);
+        }   
+
+    }
+
+}
