04b2864d44f0769ff73c22956746b086f3d73f7e
456744 <ben@kinnaird.org.uk>
2021-04-16 15:35:23 +0100

    completed menu and functionality as long as nothing has been forgotten

---
+++ basil and poppy's flower garden project/Assets/scripts/LevelChoose.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChoose : MonoBehaviour
+{
+
+    public void LevelOne()
+    {
+
+        SceneManager.LoadScene("Level 1");
+
+    }
+
+    public void LevelTwo()
+    {
+
+        SceneManager.LoadScene("Level 2");
+
+    }
+
+    public void LevelThree()
+    {
+
+        SceneManager.LoadScene("Level 3");
+
+    }
+
+}
