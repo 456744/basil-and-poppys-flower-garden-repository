8c6f63460504bdf418a99d489f2df9c860b57643
456744 <ben@kinnaird.org.uk>
2021-05-01 21:50:06 +0100

    added bug spawn mechanics (more is currently less)

---
+++ basil and poppy's flower garden project/Assets/scripts/BugScript.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BugScript : MonoBehaviour
+{
+    SpriteRenderer ourSpriteRenderer;
+
+    private void Start()
+    {
+        ourSpriteRenderer = GetComponent<SpriteRenderer>();
+        Alive = false;
+        Spawn = 50;
+    }
+
+    private float Spawn;
+    private float Time;
+    private bool Alive;
+    public float MoreBugs;
+    private float ran;
+
+    void Update()
+    {
+        if (Alive == true)
+        {
+            if (Time > 0)
+            {
+                Time = Time - 1;
+            }
+            else if (Time == 0)
+            {
+                ourSpriteRenderer.color = Color.clear;
+
+                Spawn = 50;
+
+                Alive = false;
+            }
+        }
+        else if (Alive==false)
+        {
+            if (Spawn > 0)
+            {
+                Spawn -= Mathf.Round(Random.Range(0, MoreBugs));
+            }
+            else if (Spawn == 0)
+            {
+                ourSpriteRenderer.color = Color.white;
+
+                Time = 50;
+
+                Alive = true;
+            }
+        }
+    }
+}
---
+++ basil and poppy's flower garden project/Assets/scripts/Test.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    SpriteRenderer ourSpriteRenderer;
+
+    private void Start()
+    {
+        ourSpriteRenderer = GetComponent<SpriteRenderer>();
+        Alive = false;
+        Spawn = 50;
+    }
+
+    private float Spawn;
+    private float Time;
+    private bool Alive;
+    public float MoreBugs;
+
+    void Update()
+    {
+        if (Alive == true)
+        {
+            if (Time > 0)
+            {
+                Time = Time - 1;
+                Debug.LogWarning("T1");
+            }
+            else if (Time == 0)
+            {
+                ourSpriteRenderer.color = Color.clear;
+
+                Spawn = 500;
+                Debug.LogWarning("T2");
+                Alive = false;
+            }
+        }
+        else if (Alive == false)
+        {
+            if (Spawn > 0)
+            {
+                Spawn = Spawn - 1;
+                Debug.LogWarning("S1");
+            }
+            else if (Spawn == 0)
+            {
+                ourSpriteRenderer.color = Color.white;
+
+                Time = 50;
+                Debug.LogWarning("S2");
+                Alive = true;
+            }
+        }
+    }
+}
