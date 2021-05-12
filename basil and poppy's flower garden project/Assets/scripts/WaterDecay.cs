a84675ba8fdfb41d50f891772e237f5152fc4e0a
456744 <ben@kinnaird.org.uk>
2021-05-12 15:19:01 +0100

    forgot to commit this forgot what this was

---
+++ basil and poppy's flower garden project/Assets/scripts/WaterDecay.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterDecay : MonoBehaviour
+{
+    SpriteRenderer ourSpriteRenderer;
+
+    public int Time;
+    public bool active;
+
+    private void Start()
+    {
+        ourSpriteRenderer = GetComponent<SpriteRenderer>();
+        active = false;
+    }
+
+
+    void Update()
+    {
+
+        if (active == true)
+        {
+            if (Time > 0)
+            {
+                Time = Time - 1;
+            }
+            else if (Time == 0)
+            {
+                ourSpriteRenderer.color = Color.clear;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        if (otherCollider.CompareTag("water") == true)
+        {
+            active = true;
+
+            Time = 5000;
+
+            ourSpriteRenderer.color = Color.white;
+        }
+
+    }
+}
