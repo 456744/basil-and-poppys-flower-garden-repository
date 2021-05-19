9418c960df7e4326efaa7d3cbeb9ff8a76303104
456744 <ben@kinnaird.org.uk>
2021-05-19 18:44:29 +0100

    tweaked flower growth script

--- basil and poppy's flower garden project/Assets/scripts/FlowerDecay.cs
+++ basil and poppy's flower garden project/Assets/scripts/FlowerDecay.cs
@@ -5,48 +5,54 @@ using UnityEngine;
 public class FlowerDecay : MonoBehaviour
 {
     SpriteRenderer ourSpriteRenderer;
-    private float Time;
+    public float Time;
     public float Bloom;
     private void Start()
     {
         ourSpriteRenderer = GetComponent<SpriteRenderer>();
         Time = 10;
-        Bloom = 1000;
+        Bloom = 20000000;
     }
 
 
     
     private void Decay()
     {
-        ourSpriteRenderer.color = Color.white;
+        ourSpriteRenderer.color = Color.cyan;
         gameObject.tag = "active";
-        Time = 500000;
-        Bloom = -1;
+        Time = 5000;
     }
 
 
     void Update()
     {
-        if (Time > 0)
+        if (gameObject.tag == "active")
         {
-            Time = Time - 1;
-        }
-        else if (Time == 0)
-        {
-            ourSpriteRenderer.color = Color.clear;
-            gameObject.tag = "unactive";
-            Bloom = 5000;
-        }
-        if(Bloom == 0)
-        {
-            Decay();
-        }
-        else if (Bloom > 0)
-        {
-            Bloom -= (Mathf.Round(Random.Range(0, 10)));
-            if(Bloom < 0)
+            if (Time > 0)
             {
-                Bloom = 0;
+                Time = Time - 1;
+            }
+            else if (Time == 0)
+            {
+                ourSpriteRenderer.color = Color.clear;
+                gameObject.tag = "unactive";
+                Bloom = 500000;
+            }
+        }
+        else if (gameObject.tag == "unactive")
+        {
+            if (Bloom == 0)
+            {
+                Decay();
+            }
+            else if (Bloom > 0)
+            {
+                Bloom = Bloom - (Random.Range(0, Random.Range(0, Random.Range(0, Random.Range(0, Random.Range(0, Random.Range(0, Random.Range(0, Random.Range(0, Random.Range(0, Random.Range(0, Random.Range(0, Random.Range(0, 5000000)))))))))))));
+                Debug.LogWarning(Random.Range(0, Random.Range(0, Random.Range(0, Random.Range(0, Random.Range(0, Random.Range(0, Random.Range(0, Random.Range(0, Random.Range(0, Random.Range(0, Random.Range(0, Random.Range(0, 5000000)))))))))))));
+                if (Bloom < 0)
+                {
+                    Bloom = 0;
+                }
             }
         }
     }
